004c50689b05a0f35ca20527ccd1575d889d443f
Hayden Buckley-Smith <hayden@abstack.co>
2023-04-05 16:14:20 +0100

    chore: setup next app with tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Head from 'next/head';
+
+export default function Home() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <>
+      <Head>
+        <title>Recact Playground</title>
+      </Head>
+      <div className='max-w-screen-lg mx-auto'>
+        <h1 className='text-4xl text-red-600 font-bold'>React Quickly</h1>
+        <div className='p-20'>
+          <span className='block'>Clicked {count} times!</span>
+          <div className='space-x-6'>
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={() => setCount(count - 1)}>-</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
