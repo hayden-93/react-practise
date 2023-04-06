a92886cdd3ca70583c39c175029182e7124e0bc3
Hayden Buckley-Smith <hayden@abstack.co>
2023-04-06 10:23:33 +0100

    feat: 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface PageNotFoundProps {}
+
+export default function PageNotFound({}: PageNotFoundProps) {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+    </div>
+  );
+}
